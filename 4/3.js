230a920bf826d608d1bb5157cf29003f5920461c
Dmitry <poznavshii@gmail.com>
2023-08-18 11:08:06 +0400

    add 4_homeWork

---
+++ 4/3.js
@@ -0,0 +1,42 @@
+"use strict";
+
+/*
+Используя Math.random() необходимо сгенерировать массив, содержащий 5 цифр в 
+диапазоне [0, 9].
+После создания массива необходимо вывести в консоль следующие значения:
+1. Сумму элементов массива
+2. Минимальное значение в массиве
+3. Новый массив, содержащий индексы сгенерированного выше массива, в которых 
+значение равно 3.
+Пример: Если у нас сгенерировался массив [2, 3, 5, 7, 3], то мы должны вывести 
+в консоль массив [1, 4]. Такой массив получился потому что в сгенерированном
+массиве тройки лежат под индексами 1 и 4. Если троек в сгенерированном массиве
+не окажется, значит нужно будет вывести пустой массив.
+*/
+const array = [];
+let count = 0;
+let minValue = Infinity;
+const indexOfArray = [];
+const getRandom = (min = 0, max = 9) => {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+for (let i = 0; i < 5; i++) {
+    array.push(getRandom());
+    count += array[i];
+}
+
+for (let i = 1; i < array.length; i++) {
+    minValue = Math.min(array[i - 1], array[i])
+}
+
+for (let i = 0; i < array.length; i++) {
+    if (array[i] === 3) {
+        indexOfArray.push(i);
+    }
+}
+
+console.log(array);
+console.log(count);
+console.log(minValue);
+console.log(indexOfArray);
